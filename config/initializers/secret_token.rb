c33b9554900219ccd1f15f0d35d8a69bac68040c
honeycomb80 <honeycomb8080@gmail.com>
2014-05-16 14:36:46 -0700

    setup app, user, added devise, generated devise User, model, and views, migrated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JobSearchTasker::Application.config.secret_key_base = '9ce8d7e189a80ef826e831a283e82eb2c6975dfb74fdead9c0dcde220cf1b17f713bff133bef891be5df9cf6402a56af25f74e4e2188ab59a361727047814f23'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JobSearchTasker::Application.config.session_store :cookie_store, key: '_job_search_tasker_session'
